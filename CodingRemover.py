9aff93498257f156ded6968c69c2af97c2287943
JohnLouloudis <45006797+JohnLouloudis@users.noreply.github.com>
2019-08-26 14:20:16 +0300

    Update CodingRemover.py

--- CodingRemover.py
+++ CodingRemover.py
@@ -1,3 +1,6 @@
+# The Splash pipeline was created at the Hellenic Center of Marine Research (HMCR), in Crete (August 2019).
+# It's developers were John Louloudis, Tereza Manousaki and Alexandros Tsakogiannis.
+
 # Import libraries.
 import pandas as pd
 import sys
